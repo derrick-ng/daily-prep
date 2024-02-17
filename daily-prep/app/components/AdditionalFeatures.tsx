446aeb1d076a6b9a389f9cea22ee53a3f668d44d
derrick-ng <ng.derrick1@hotmail.com>
2024-02-17 15:22:20 -0800

    test

--- daily-prep/app/components/AdditionalFeatures.tsx
+++ daily-prep/app/components/AdditionalFeatures.tsx
@@ -1,46 +1,61 @@
-import React from 'react'
+import React from "react";
 
 const AdditionalFeatures = () => {
   return (
-    <form id="additional-features-container">
-      <p>Select everything you want to be sent</p>
+    <div>
+      <h3>Select everything you want to be sent</h3>
 
-      <div className="additional-features">
-        <input type="checkbox" id="weather-checkbox" />
-        <label >Do you want to be sent the weather?</label>
-      </div>
-
-      <div className="additional-features" id="email-features-container">
-        <input type="checkbox" id="email-checkbox" />
-        <label>Email?</label>
-        <input type="text" placeholder="personal email" />
-        <input type="text" placeholder="name priority senders" />
-      </div>
-
-      <div className="additional-features">
-        <input type="checkbox" id="eta-checkbox" />
-        <label>Eta to Destination? </label>
-        <input type="text" placeholder="Start" />
-        <input type="text" placeholder="Destination" />
-        <div>
-          <fieldset>
-            <legend>Mode of Transportation (default drive)</legend>
-            <label> <input type="radio" name="type-transportation" value="1" checked />Drive </label>
-            <label> <input type="radio" name="type-transportation" value="2" />Public Transport </label>
-            <label> <input type="radio" name="type-transportation" value="3" />Walk </label>
-            <label> <input type="radio" name="type-transportation" value="4" />Bike </label>
-          </fieldset>
+      <form id="additional-features-container" method="POST">
+        <div className="additional-features">
+          <input type="checkbox" id="weather-checkbox" />
+          <label>Do you want to be sent the weather?</label>
         </div>
-      </div>
 
-      <div className="additional-features">
-        <p>Enter phone number and time you would like to receive text (default 8:30 am)</p>
-        <input type="text" placeholder="phone number" />
-        <input type="text" placeholder="time" />
-        <button>Save</button>
-      </div>
-    </form>
-  )
-}
+        <div className="additional-features" id="email-features-container">
+          <input type="checkbox" id="email-checkbox" />
+          <label>Email?</label>
+          <input type="text" placeholder="personal email" />
+          <input type="text" placeholder="name priority senders" />
+        </div>
 
-export default AdditionalFeatures
+        <div className="additional-features">
+          <input type="checkbox" id="eta-checkbox" />
+          <label>Eta to Destination? </label>
+          <input type="text" placeholder="Start" />
+          <input type="text" placeholder="Destination" />
+          <div>
+            <fieldset>
+              <legend>Mode of Transportation (default drive)</legend>
+              <label>
+                <input type="radio" name="type-transportation" value="1" checked />
+                Drive
+              </label>
+              <label>
+                <input type="radio" name="type-transportation" value="2" />
+                Public Transport
+              </label>
+              <label>
+                <input type="radio" name="type-transportation" value="3" />
+                Walk
+              </label>
+              <label>
+                <input type="radio" name="type-transportation" value="4" />
+                Bike
+              </label>
+            </fieldset>
+          </div>
+        </div>
+
+        <div className="additional-features">
+          <p>Enter phone number and time you would like to receive text (default 8:30 am)</p>
+          <input type="text" placeholder="phone number" />
+          <input type="text" placeholder="time" />
+        </div>
+
+        <button type="submit">Save</button>
+      </form>
+    </div>
+  );
+};
+
+export default AdditionalFeatures;
