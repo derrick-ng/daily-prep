52019ca20800558c1644f350d7bb126a1f8a48e1
derrick-ng <ng.derrick1@hotmail.com>
2024-02-19 03:48:18 -0800

    cleaned up code, do AdditionalFeatures next

--- daily-prep/app/components/TaskList.tsx
+++ daily-prep/app/components/TaskList.tsx
@@ -2,18 +2,11 @@ import React from 'react'
 import AddForm from './AddForm';
 import { Button } from '@radix-ui/themes';
 
-interface formContent {
-  task: string;
-  weather: boolean;
-  email: boolean;
-  emailPriority: string;
-}
-
 const TaskList = () => {  
   return (
     <div className='w-1/2 text-2xl border-black border-4'>
       <h3 className='text-center'>Add New Tasks</h3>
-      
+
       <AddForm />
       <div>
        <ul>
