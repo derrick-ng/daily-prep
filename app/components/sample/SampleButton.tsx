0986c42378113d13689091f8619ea7fb597bb184
derrick-ng <ng.derrick1@hotmail.com>
2025-03-31 17:15:08 -0700

    Fix deployment issues

--- app/components/sample/SampleButton.tsx
+++ app/components/sample/SampleButton.tsx
@@ -1,10 +1,7 @@
 "use client";
 
-import React, { useEffect, useState } from "react";
-import { WeatherData } from "@/app/types/Weather";
-import { TrafficData } from "@/app/types/Traffic";
+import React from "react";
 import axios from "axios";
-import { Todo } from "@/app/types/Todo";
 
 const SampleButton = ({ userId }: { userId: number | null }) => {
   const handleSampleClick = async () => {
@@ -19,6 +16,7 @@ const SampleButton = ({ userId }: { userId: number | null }) => {
           userId,
         },
       });
+      console.log("result of send push noti", sendPushNotification);
     } catch (error) {
       console.error("error finding form data", error);
     }
