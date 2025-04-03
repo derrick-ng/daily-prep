537ff5a3e5d13e47a8ff02cacb7f5b4f8b2a039f
derrick-ng <ng.derrick1@hotmail.com>
2025-04-03 15:39:12 -0700

    Move service worker file to work with previously set up next.config.ts settings

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,23 @@
 import nextPwa from "next-pwa";
 
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        hostname: "avatars.githubusercontent.com",
+        protocol: "https" as const,
+      },
+      {
+        hostname: "lh3.googleusercontent.com",
+        protocol: "https" as const,
+      },
+      {
+        hostname: "zpapagojlfddtjyciyum.supabase.co",
+        protocol: "https" as const,
+      },
+    ],
+  },
+};
 
 const withPWA = nextPwa({
   dest: "public",
