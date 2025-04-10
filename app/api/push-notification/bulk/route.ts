a7b4c37fd8ebc43863d4da5bae7a826f0de12fc5
derrick-ng <ng.derrick1@hotmail.com>
2025-04-10 16:07:08 -0700

    Implement endpoint to send push notifications to all agreed users

--- app/api/push-notification/bulk/route.ts
+++ app/api/push-notification/bulk/route.ts
@@ -1,3 +1,22 @@
+import prisma from "@/lib/prismaClient";
+import { sendPushNotificationToUser } from "@/lib/pushService";
+
 export async function GET() {
-    
+  try {
+    const allUsers = await prisma.pushSubscription.findMany({
+      where: {
+        enabled: true,
+      },
+    });
+
+    await Promise.all(
+      allUsers.map(({ userId, endpoint }) => {
+        sendPushNotificationToUser({ userId, endpoint });
+      })
+    );
+
+    return Response.json({ successfullySentPushNotifications: true }, { status: 200 });
+  } catch (error) {
+    return Response.json({ error }, { status: 400 });
+  }
 }
